0e933e319fc492a897f7d230c125112b5d8e68b3
Michael P. Jung <michael.jung@terreon.de>
2022-03-09 21:10:15 +0100

    Add dummy sprite and camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::{App, Commands, Component, Entity, Query, Transform}, DefaultPlugins};
+use bevy::{prelude::{App, Commands, Component, Entity, Query, Transform, Color, OrthographicCameraBundle, UiCameraBundle}, DefaultPlugins, sprite::{SpriteBundle, Sprite}, math::Vec3};
 
 #[derive(Debug, Component)]
 struct Player {
@@ -11,18 +11,28 @@ struct Position {
     y: f32,
 }
 
-fn print_position_system(query: Query<(Option<&Player>, &Position)>) {
-    for (player, pos) in query.iter() {
+fn print_position_system(mut query: Query<(Option<&Player>, &mut Position)>) {
+    for (player, mut pos) in query.iter_mut() {
+        pos.y += 1.0;
         println!("{:?}: {}:{}", player, pos.x, pos.y);
     }
 }
 
 fn create_entities(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
     commands
-        .spawn()
-        .insert(Position { x: 42.0, y: 0.0 })
-        .insert(Player {
-            name: "Benny".into(),
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(255.0, 255.0, 255.0),
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(10.0, 10.0, 0.0),
+                ..Default::default()
+            },
+            ..Default::default()
         });
     commands
         .spawn()
