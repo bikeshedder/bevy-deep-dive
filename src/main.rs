9ca143cc4697027900429a215396135d84179f22
Michael P. Jung <michael.jung@terreon.de>
2022-03-10 10:57:54 +0100

    Add bevy_spicy_aseprite and animate character

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use bevy::{
     core::FixedTimestep,
     input::Input,
-    math::{Vec2, Vec3},
+    math::Vec3,
     prelude::{
-        App, AssetServer, Assets, Color, Commands, Component, Entity, KeyCode,
-        OrthographicCameraBundle, Query, Res, ResMut, SystemSet, Transform, UiCameraBundle,
+        App, Commands, Component, KeyCode, OrthographicCameraBundle, Query, Res, SystemSet,
+        Transform, UiCameraBundle,
     },
-    sprite::{Sprite, SpriteBundle, SpriteSheetBundle, TextureAtlas, TextureAtlasSprite},
     DefaultPlugins,
 };
+use bevy_spicy_aseprite::{
+    AsepriteAnimation, AsepriteAnimationState, AsepriteBundle, AsepritePlugin,
+};
+
+mod sprites {
+    use bevy_spicy_aseprite::aseprite;
+    aseprite!(pub Player, "assets/player-base.aseprite");
+}
+
+const TIME_STEP: f32 = 1.0 / 60.0;
 
 #[derive(Debug, Component)]
 struct Player {
@@ -21,41 +30,49 @@ struct Player {
 
 fn player_input(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform, &mut TextureAtlasSprite)>,
+    mut query: Query<(&Player, &mut Transform, &mut AsepriteAnimation)>,
 ) {
-    for (player, mut transform, mut sprite) in query.iter_mut() {
-        sprite.index = (sprite.index + 1) % 4;
+    for (player, mut transform, mut animation) in query.iter_mut() {
         if keyboard_input.pressed(player.key_left) {
-            transform.translation.x -= 1.0;
+            if !animation.is_tag(sprites::Player::tags::WALK_RIGHT) {
+                *animation = AsepriteAnimation::from(sprites::Player::tags::WALK_RIGHT);
+            }
+            transform.translation.x -= 5.0;
         }
         if keyboard_input.pressed(player.key_right) {
-            transform.translation.x += 1.0;
+            if !animation.is_tag(sprites::Player::tags::WALK_RIGHT) {
+                *animation = AsepriteAnimation::from(sprites::Player::tags::WALK_RIGHT);
+            }
+            transform.translation.x += 5.0;
         }
         if keyboard_input.pressed(player.key_down) {
-            transform.translation.y -= 1.0;
+            if !animation.is_tag(sprites::Player::tags::WALK_DOWN) {
+                *animation = AsepriteAnimation::from(sprites::Player::tags::WALK_DOWN);
+            }
+            transform.translation.y -= 5.0;
         }
         if keyboard_input.pressed(player.key_up) {
-            transform.translation.y += 1.0;
+            if !animation.is_tag(sprites::Player::tags::WALK_UP) {
+                *animation = AsepriteAnimation::from(sprites::Player::tags::WALK_UP);
+            }
+            transform.translation.y += 5.0;
         }
     }
 }
 
-fn create_entities(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
+fn create_entities(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
 
-    let texture_handle = asset_server.load("player-base.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 12, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
     commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(6.0)),
+        .spawn_bundle(AsepriteBundle {
+            aseprite: sprites::Player::sprite(),
+            animation: AsepriteAnimation::from(sprites::Player::tags::WALK_UP),
+            transform: Transform {
+                scale: Vec3::splat(4.0),
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..Default::default()
+            },
             ..Default::default()
         })
         .insert(Player {
@@ -67,18 +84,14 @@ fn create_entities(
         });
 }
 
-const TIME_STEP: f32 = 1.0 / 60.0;
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(AsepritePlugin)
         .add_startup_system(create_entities)
-        //.add_system(player_input)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                //.with_system(paddle_movement_system)
-                //.with_system(ball_collision_system)
                 .with_system(player_input),
         )
         .add_system(bevy::input::system::exit_on_esc_system)
